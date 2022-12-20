169fe89a604297740ef7fd9acd3ad5748f0e5f10
Igor Nazim <igor.mainevent@gmail.com>
2022-12-20 20:32:38 +0200

    Revert "Добавление вотчера"
    This reverts commit acd36445c80d41b47f1bbc4dfd9b37a1ca1b5cf6.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-import yup from 'yup';
-import watchedState from './render.js';
 import './styles.scss';
 import 'bootstrap';
 
-const btn = document.querySelector('.h-100');
-btn.addEventListener('click', (e) => {
-  watchedState.urlInput.data.url = e.target.value;
-});
+console.log('Hello, world!!!!');
