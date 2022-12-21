8e186c92d507082838d5ac00cdb84b5b991bb0b8
Igor Nazim <igor.mainevent@gmail.com>
2022-12-21 19:01:20 +0200

    Настройка валидации url

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import './styles.scss';
 import 'bootstrap';
+import { string } from 'yup';
+import watchedState from './render.js';
 
-console.log('Hello, world!!!!');
+const schema = string().url().nullable().notOneOf(watchedState.inputUrl.data.urls);
+const form = document.querySelector('.rss-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const url = formData.get('url');
+  schema.isValid(url)
+    .then((data) => {
+      if (data === true) {
+        watchedState.inputUrl.data.urls.push(url);
+        watchedState.inputUrl.state = 'valid';
+      } else if (data === false) {
+        watchedState.inputUrl.state = 'invalid';
+      }
+    })
+    .catch((err) => {
+      console.log('error!!!', err);
+    });
+});
---
+++ src/render.js
@@ -0,0 +1,31 @@
+import onChange from 'on-change';
+
+const renderValidInput = () => {
+  const input = document.querySelector('#url-input');
+  const form = document.querySelector('.rss-form');
+  input.style.border = '';
+  form.reset();
+  input.focus();
+};
+const renderErrors = () => {
+  const input = document.querySelector('#url-input');
+  input.style.border = 'thick solid red';
+};
+const state = {
+  inputUrl: {
+    state: 'valid',
+    data: {
+      urls: [],
+    },
+    errors: [],
+  },
+};
+const watchedState = onChange(state, (path, value, previousValue) => {
+  if (watchedState.inputUrl.state === 'valid') {
+    renderValidInput();
+  } else if (watchedState.inputUrl.state === 'invalid') {
+    renderErrors();
+  }
+});
+
+export default watchedState;
